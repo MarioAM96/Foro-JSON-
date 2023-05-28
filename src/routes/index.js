31f902ee61f14ec69660a20ae4bd026d03b9e43b
Mario <marioandres.1996@icloud.com>
2023-05-27 19:12:25 -0500

    Save data

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,15 @@ const router = Router ();
 
 router.get('/', (req, res) => {
     res.render('index.ejs');
-  });
+});
 
 router.get('/new-entry', (req, res) => {
     res.render('new-entry');
-  });
+});
+
+router.post('/new-entry', (req, res) => {
+    console.log(req.body);
+    res.send('received');
+});
 
 module.exports = router;
