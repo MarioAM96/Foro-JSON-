f835e22deae98bfa7aee1b174e03551722e54484
Mario <marioandres.1996@icloud.com>
2023-05-27 19:52:27 -0500

    Save JSON

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,13 @@
 const { Router } = require ('express');
 const router = Router ();
+const fs = require('fs');
+
+const comments = [];
 
 router.get('/', (req, res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', {
+        comments
+    });
 });
 
 router.get('/new-entry', (req, res) => {
@@ -10,7 +15,24 @@ router.get('/new-entry', (req, res) => {
 });
 
 router.post('/new-entry', (req, res) => {
-    console.log(req.body);
+    const { title, comment1, image } = req.body;
+
+    if (!title || !comment1|| !image ) {
+        res.status(400).send("Los campos son obligatorios");
+        return;
+      }
+
+    let newComment = {
+        title, 
+        comment1, 
+        image 
+    }
+
+    comments.push(newComment);
+
+    const json_comments = JSON.stringify(comments);
+    fs.writeFileSync('src/comments.json', json_comments, 'utf-8')
+
     res.send('received');
 });
 
